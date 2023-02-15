2c2b1e10b374df8efdbf1f6147c6277fa98f7bb3
Hermes <hermeschacon@live.com.ar>
2023-02-15 10:30:39 -0300

    navbar y countdown

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+import s from './Navbar.module.css'
+import MNlogo from '../../assets/logo-2020.png'
+import CountDownTimer from './CountDownTimer'
+
 
 export default function NavBar() {
 
-
+    return <>
+        <div className={s.topnav} id="myTopnav">
+            <a href="#home" >
+                <img src={MNlogo} alt="La Media Naranja" />
+            </a>
+            <div className={s.timers}>
+                <CountDownTimer date="03/24/2023" />
+            </div>
+            <div className={s.userProfile}>
+                <a >User Name</a>
+            </div>
+        </div>
+    </>
 }
