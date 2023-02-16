cd0d38de478a310da596b3537d7d9ee32d56bfc3
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-02-16 17:36:47 -0500

    Fixes files for app running...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import NavBar from "./components/Navbar/Navbar";
-import Home from './components/Home/Home'
+import Home from "./components/Home/Home";
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
           <Route exact path="/" element={<NavBar />} />
         </Routes>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/admin" element={<Dashboard />} /
+          <Route exact path="/" element={<Home />} />
+          <Route path="/admin" element={<Dashboard />} />
         </Routes>
       </div>
     </div>
