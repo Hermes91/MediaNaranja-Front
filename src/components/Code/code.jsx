45d1abefcea4443d67e4e32e25edd57084982fe8
Hermes <hermeschacon@live.com.ar>
2023-02-17 13:22:49 -0300

    agregado delimitador de caracteres maxlength=13

--- src/components/Code/code.jsx
+++ src/components/Code/code.jsx
@@ -6,7 +6,8 @@ import s from '../Code/code.module.css'
 function validate(input) {
     const error = {}
     const isBlankSpace = new RegExp("^\\s+$")
-    if (!input.codigo || isBlankSpace.test(input.codigo)) {error.codigo = 'Ingrese un codigo valido'}
+    if (!input.codigo || isBlankSpace.test(input.codigo)) { error.codigo = 'Ingrese un codigo valido' }
+    else if (input.codigo.trim().length > 13) error.codigo = `(${input.codigo.trim().length}/13)`
     return error;
 }
 
@@ -29,7 +30,7 @@ export default function CodeRegister({ handleClose }) {
 
     const handleChange = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value })
-        const validacion=validate(input)
+        const validacion = validate(input)
         setErr(validacion)
     }
 
@@ -42,7 +43,7 @@ export default function CodeRegister({ handleClose }) {
                 <div className={s.codeBody}>
                     <h2>Ingresa tu Código aqui</h2>
                     <form onSubmit={handleSubmit} className={s.codeForm}>
-                        <input value={input.codigo} name="codigo" onChange={handleChange} type="text" />
+                        <input value={input.codigo} name="codigo" onChange={handleChange} type="text" maxlength="13" />
                         {err.codigo && <span className={s.formerror}>{err.codigo}</span>}
 
                         <button disabled={isButtonDisabled()} type='submit'>Subir mi Código</button>
