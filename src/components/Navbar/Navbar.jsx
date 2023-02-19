f3a0ff3f1f51ec3e4165365492999bdac3fe05bd
bosxch <amparocardosobosch@gmail.com>
2023-02-19 19:36:15 -0300

    aceder secction in navbar if there isn´t a logged user

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,15 @@ import MNlogo from '../../assets/logo-2020.png'
 import CountDownTimer from './CountDownTimer'
 import { Backdrop } from "@mui/material";
 import CodeReg from "../Code/code";
+import UserForm from "../UserForm/UserForm";
+import IntroDNI from "../introDNI/introDni";
 
 
 
 export default function NavBar() {
 
     const [open, setOpen] = useState(false);
+    const user = JSON.parse(localStorage.getItem("user"))
     const handleToggle = () => {
         setOpen(!open)
     };
@@ -22,13 +25,28 @@ export default function NavBar() {
             <div className={s.timers}>
                 <CountDownTimer date="03/23/2023" />
             </div>
+            {user? 
+            <>
             <div className={s.userProfile} onClick={handleToggle}>
-                <h3>Ingresar mis cupones</h3>
-            </div>
-            <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={open}>
-                <CodeReg handleClose={() => setOpen(false)} />
-            </Backdrop>
+            <h3>Ingresar mis cupones</h3>
+        </div>
+        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={open}>
+            <CodeReg handleClose={() => setOpen(false)} />
+        </Backdrop>
+        </>
+        :
+        <>
+        <div className={s.userProfile} onClick={handleToggle}>
+            <h3>Acceder</h3>
+        </div>
+        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={open}>
+            <IntroDNI handleClose={() => setOpen(false)} />
+        </Backdrop>
+        </>
+        }
+            
         </div>
     </>)
 }
