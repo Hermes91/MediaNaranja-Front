a622dba9e42fbb74715241c47d33699a0faf4355
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-02-18 20:55:45 -0500

    Totals functionality on dashboard

--- src/components/dashboard/Deposits.js
+++ src/components/dashboard/Deposits.js
@@ -1,19 +1,32 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Title from "./Title";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getTickets } from "../../redux/actions/actionIndex";
+import { getUsers } from "../../redux/actions/actionIndex";
 
 export default function Deposits() {
+  const dispatch = useDispatch();
+  const tickets = useSelector((state) => state.allTickets);
+  const users = useSelector((state) => state.allUsers);
+
+  useEffect(() => {
+    !tickets.length && dispatch(getTickets());
+    !users.length && dispatch(getUsers());
+  }, [tickets, users, dispatch]);
+
   return (
     <React.Fragment>
       <Title>Total de cupones</Title>
       <Typography component="p" variant="h4">
-        345
+        {tickets.length}
       </Typography>
       <p></p>
 
       <Title>Usuarios registrados</Title>
       <Typography component="p" variant="h4">
-        57
+        {users.length}
       </Typography>
     </React.Fragment>
   );
