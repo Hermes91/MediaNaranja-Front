20da4bd7324a23830fb74eeacbcd0b6a7dff0703
Hermes <hermeschacon@live.com.ar>
2023-02-19 15:04:22 -0300

    boton inicio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,26 @@
-import { React } from "react";
+import { React, useState } from "react";
 import Banner from '../Banner/Banner'
 import Condition from "../Conditions/condition";
 import Footer from "../Footer/Footer";
+import IntroDNI from "../introDNI/introDni"
+import { Backdrop } from "@mui/material";
+
 import s from './Home.module.css'
 
+
 export default function Home() {
+
+    const [open, setOpen] = useState(true);
 
 
     return (
         <>
+            <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}>
+                <div className={s.cedula}>
+                    <IntroDNI handleClose={() => setOpen(false)} />
+                </div>
+            </Backdrop>
             <div className={s.banner}>
                 <Banner />
             </div>
