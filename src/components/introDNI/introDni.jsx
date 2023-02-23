ff7fbfd331bf326fb437d3ccec2dbbcb54fa6108
Hermes <hermeschacon@live.com.ar>
2023-02-23 18:58:03 -0300

    color toast y font size

--- src/components/introDNI/introDni.jsx
+++ src/components/introDNI/introDni.jsx
@@ -4,31 +4,36 @@ import { searchByDocument, getUsers } from '../../redux/actions/actionIndex'
 import s from '../introDNI/introDni.module.css'
 import { toast } from 'react-toastify';
 
+
 ///////  valida el DNI ingresado  //////
 function validate(input, allUsers) {
     const error = {}
     const isBlankSpace = new RegExp("^\\s+$")
-    const isDNI = /^[0-9]{6,11}$/ 
+    const isDNI = /^[0-9]{6,11}$/
     if (!input.numDocumento || isBlankSpace.test(input.numDocumento)) error.numDocumento = 'Ingrese su n° de documento'
     else if (!isDNI.test(input.numDocumento)) error.numDocumento = 'Ingrese un documento valido'
     return error;
 }
 
 export default function IntroDNI({ handleClose }) {
+
     const [err, setErr] = useState({})
     const allUsers = useSelector(state => state.allUsers)
+  
+
     let dispatch = useDispatch()
-    
+
     useEffect(() => {
         !allUsers.length && dispatch(getUsers())
     }, [allUsers, dispatch])
-    
+
+
     const [input, setInput] = useState({
         numDocumento: "",
     })
 
-    var yaExiste = allUsers.find(u => u.numDocumento == input?.numDocumento)
-    
+    var yaExiste = allUsers.find(u => u.numDocumento === input?.numDocumento)
+
     const isButtonDisabled = () => {
         if (Object.keys(err).length || !input.numDocumento) return true
         else return false
@@ -42,7 +47,7 @@ export default function IntroDNI({ handleClose }) {
         if (yaExiste && !Object.keys(err).length) {
             localStorage.setItem("user", JSON.stringify(yaExiste));
             window.location.reload()
-        } else{
+        } else {
             toast.warn("Usted no está registrado, haga click en Únete Ahora")
         }
     }
@@ -56,6 +61,7 @@ export default function IntroDNI({ handleClose }) {
 
     return (
         <>
+
             <div className={s.dniContainer}>
                 <h3 onClick={handleClose}>
                     X
@@ -66,10 +72,11 @@ export default function IntroDNI({ handleClose }) {
                         <input value={input.numDocumento} name="numDocumento" onChange={handleChange} type="number" maxLength="10" />
                         {err.numDocumento && <span className={s.formerror}>{err.numDocumento}</span>}
                         <button onClick={handleClose} disabled={isButtonDisabled()} type='submit'>Acceder</button>
-                        {!err.numDocumento && !yaExiste ? <a onClick={handleClose} href="#user_form">Si todavía no se ha registrado haga click acá</a>: null}
+                        {!err.numDocumento && !yaExiste ? <a onClick={handleClose} href="#user_form">Regístrece aquí</a> : null}
                     </form>
                 </div>
             </div>
+
         </>
     )
 }
