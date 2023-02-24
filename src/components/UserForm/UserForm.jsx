71f1686bcd564add0e71ac5059a8d3588741b000
Juan Manuel Caicedo <juanamanuelcaicedo@hotmail.com>
2023-02-24 10:28:43 -0500

    minimal text corrections...

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,97 +1,139 @@
 import React, { useState } from "react";
-import validate from '../UserForm/validate'
-import s from './UserForm.module.css'
+import validate from "../UserForm/validate";
+import s from "./UserForm.module.css";
 import { postUser } from "../../redux/actions/actionIndex";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
-
 export default function UserForm({ handleClose }) {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const allUsers = useSelector(state => state.allUsers)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const allUsers = useSelector((state) => state.allUsers);
 
-    const [input, setInput] = useState({
-        email: "",
-        nombre: "",
-        numDocumento: "",
-        direccion: "",
-        telephone: "",
-    })
-    const [err, setErr] = useState({})
+  const [input, setInput] = useState({
+    email: "",
+    nombre: "",
+    numDocumento: "",
+    direccion: "",
+    telephone: "",
+  });
+  const [err, setErr] = useState({});
 
-    var yaExiste = allUsers.find((u) => u.numDocumento == input.numDocumento)
+  var yaExiste = allUsers.find((u) => u.numDocumento == input.numDocumento);
 
-    const isButtonDisabled = () => {
-        if (!!Object.keys(err).length || !input.email.length) return true
-        else if (yaExiste) return true
-        else return false
-    }
+  const isButtonDisabled = () => {
+    if (!!Object.keys(err).length || !input.email.length) return true;
+    else if (yaExiste) return true;
+    else return false;
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const newUser = {
-            email: input.email,
-            nombre: input.nombre,
-            numDocumento: input.numDocumento,
-            direccion: input.direccion,
-            telephone: input.telephone,
-        }
-        dispatch(postUser(newUser))
-        localStorage.setItem("user", JSON.stringify(newUser));
-        handleClose()
-        setInput({
-            email: "",
-            nombre: "",
-            numDocumento: "",
-            direccion: "",
-            telephone: "",
-        })
-        window.location.reload()
-        navigate('/')
-    }
-    const handleChange = (e) => {
-        setInput({ ...input, [e.target.name]: e.target.value })
-        setErr(validate({ ...input, [e.target.name]: e.target.value }))
-    }
-    return <>
-        <div className={s.formContainer}>
-            <h3 onClick={handleClose}>
-                X
-            </h3>
-            <div className={s.formBody}>
-                <h2>Completa el formulario para cargar tus cupones!</h2>
-                <h5>Los campos con * son obligatorios</h5>
-                <form onSubmit={handleSubmit} className={s.formulario}>
-                    <div className={s.formItem}>
-                        <label>Nombre*: </label>
-                        <input value={input.nombre} name='nombre' onChange={handleChange} type='text' placeholder='Ingrese su nombre' />
-                        {err.nombre && <span className={s.formerror}>{err.nombre}</span>}
-                    </div>
-                    <div className={s.formItem}>
-                        <label>E-mail*: </label>
-                        <input value={input.email} name='email' onChange={handleChange} type='text' placeholder='Ingrese su mail' />
-                        {err.email && <span className={s.formerror}>{err.email}</span>}
-                    </div>
-                    <div className={s.formItem}>
-                        <label>N° de Documento*: </label>
-                        <input value={input.numDocumento} name='numDocumento' onChange={handleChange} type='text' placeholder='Ingrese su cedula' />
-                        {err.numDocumento && <span className={s.formerror}>{err.numDocumento}</span>}
-                    </div>
-                    <div className={s.formItem}>
-                        <label>Direccion: </label>
-                        <input value={input.direccion} name='direccion' onChange={handleChange} type='text' placeholder='Indique su direccion' />
-                    </div>
-                    <div className={s.formItem}>
-                        <label>Telefono*: </label>
-                        <input value={input.telephone} name='telephone' onChange={handleChange} type='text' placeholder='Su número de telefono' />
-                        {err.telephone && <span className={s.formerror}>{err.telephone}</span>}
-                    </div>
-                    {yaExiste? <span className={s.formerror}>Este documento ya fue registrado</span>: null}
-                    <button onClick={handleClose} disabled={isButtonDisabled()} type='submit' >Enviar Formulario</button>
-                </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      email: input.email,
+      nombre: input.nombre,
+      numDocumento: input.numDocumento,
+      direccion: input.direccion,
+      telephone: input.telephone,
+    };
+    dispatch(postUser(newUser));
+    localStorage.setItem("user", JSON.stringify(newUser));
+    handleClose();
+    setInput({
+      email: "",
+      nombre: "",
+      numDocumento: "",
+      direccion: "",
+      telephone: "",
+    });
+    window.location.reload();
+    navigate("/");
+  };
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    setErr(validate({ ...input, [e.target.name]: e.target.value }));
+  };
+  return (
+    <>
+      <div className={s.formContainer}>
+        <h3 onClick={handleClose}>X</h3>
+        <div className={s.formBody}>
+          <h2>¡Regístrate para ingresar tus cupones!</h2>
+          <form onSubmit={handleSubmit} className={s.formulario}>
+            <div className={s.formItem}>
+              <label>Nombre*: </label>
+              <input
+                value={input.nombre}
+                name="nombre"
+                onChange={handleChange}
+                type="text"
+                placeholder="Ingrese su nombre"
+              />
+              {err.nombre && <span className={s.formerror}>{err.nombre}</span>}
             </div>
+            <div className={s.formItem}>
+              <label>E-mail*: </label>
+              <input
+                value={input.email}
+                name="email"
+                onChange={handleChange}
+                type="text"
+                placeholder="Ingrese su mail"
+              />
+              {err.email && <span className={s.formerror}>{err.email}</span>}
+            </div>
+            <div className={s.formItem}>
+              <label>N° de Documento*: </label>
+              <input
+                value={input.numDocumento}
+                name="numDocumento"
+                onChange={handleChange}
+                type="text"
+                placeholder="Ingrese su cedula"
+              />
+              {err.numDocumento && (
+                <span className={s.formerror}>{err.numDocumento}</span>
+              )}
+            </div>
+            <div className={s.formItem}>
+              <label>Dirección: </label>
+              <input
+                value={input.direccion}
+                name="direccion"
+                onChange={handleChange}
+                type="text"
+                placeholder="Indique su direccion"
+              />
+            </div>
+            <div className={s.formItem}>
+              <label>Teléfono*: </label>
+              <input
+                value={input.telephone}
+                name="telephone"
+                onChange={handleChange}
+                type="text"
+                placeholder="Su número de telefono"
+              />
+              {err.telephone && (
+                <span className={s.formerror}>{err.telephone}</span>
+              )}
+            </div>
+            {yaExiste ? (
+              <span className={s.formerror}>
+                ¡Tu cédula ya está registrada!
+              </span>
+            ) : null}
+            <button
+              onClick={handleClose}
+              disabled={isButtonDisabled()}
+              type="submit"
+            >
+              REGISTRARME
+            </button>
+          </form>
         </div>
+      </div>
     </>
+  );
 }
--- src/components/introDNI/introDni.jsx
+++ src/components/introDNI/introDni.jsx
@@ -1,105 +1,119 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import { searchByDocument, getUsers } from '../../redux/actions/actionIndex'
-import s from '../introDNI/introDni.module.css'
-import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from "react-redux";
+import { searchByDocument, getUsers } from "../../redux/actions/actionIndex";
+import s from "../introDNI/introDni.module.css";
+import { toast } from "react-toastify";
 import UserForm from "../UserForm/UserForm";
 import { Backdrop } from "@mui/material";
 
-
 ///////  valida el DNI ingresado  //////
 function validate(input, allUsers) {
-    const error = {}
-    const isBlankSpace = new RegExp("^\\s+$")
-    const isDNI = /^[0-9]{6,10}$/
-    if (!input.numDocumento || isBlankSpace.test(input.numDocumento)) error.numDocumento = 'Ingrese su n° de documento'
-    else if (!isDNI.test(input.numDocumento)) error.numDocumento = 'Ingrese un documento valido'
-    return error;
+  const error = {};
+  const isBlankSpace = new RegExp("^\\s+$");
+  const isDNI = /^[0-9]{6,10}$/;
+  if (!input.numDocumento || isBlankSpace.test(input.numDocumento))
+    error.numDocumento = "Ingrese su n° de documento";
+  else if (!isDNI.test(input.numDocumento))
+    error.numDocumento = "Ingrese un documento valido";
+  return error;
 }
 
 export default function IntroDNI({ handleClose }) {
+  const [err, setErr] = useState({});
+  const allUsers = useSelector((state) => state.allUsers);
 
-    const [err, setErr] = useState({})
-    const allUsers = useSelector(state => state.allUsers)
+  const [open, setOpen] = useState(false);
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
-    const [open, setOpen] = useState(false);
-    const handleToggle = () => {
-        setOpen(!open);
-    };
-  
-    let dispatch = useDispatch()
+  let dispatch = useDispatch();
 
-    useEffect(() => {
-        !allUsers.length && dispatch(getUsers())
-    }, [allUsers, dispatch])
+  useEffect(() => {
+    !allUsers.length && dispatch(getUsers());
+  }, [allUsers, dispatch]);
 
+  const [input, setInput] = useState({
+    numDocumento: "",
+  });
 
-    const [input, setInput] = useState({
-        numDocumento: "",
-    })
+  var yaExiste = allUsers.find((u) => u.numDocumento === input?.numDocumento);
 
-    var yaExiste = allUsers.find(u => u.numDocumento === input?.numDocumento)
-    
-    const isButtonDisabled = () => {
-        if (Object.keys(err).length || !input.numDocumento) return true
-        else if (!yaExiste) return true
-        return false
+  const isButtonDisabled = () => {
+    if (Object.keys(err).length || !input.numDocumento) return true;
+    else if (!yaExiste) return true;
+    return false;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searchByDocument(Number(input.numDocumento)));
+    const validacion = validate(input, allUsers, yaExiste);
+    setErr(validacion);
+    if (yaExiste && !Object.keys(err).length) {
+      localStorage.setItem("user", JSON.stringify(yaExiste));
+      window.location.reload();
+    } else {
+      toast.warn("Usted no está registrado, haga click en Únete Ahora");
     }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatch(searchByDocument(Number(input.numDocumento)))
-        const validacion = validate(input, allUsers, yaExiste)
-        setErr(validacion)
-        if (yaExiste && !Object.keys(err).length) {
-            localStorage.setItem("user", JSON.stringify(yaExiste));
-            window.location.reload()
-        } else {
-            toast.warn("Usted no está registrado, haga click en Únete Ahora")
-        }
-    }
+  };
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        setInput({ ...input, [e.target.name]: Number(e.target.value) })
-        const validacion = validate(input, allUsers)
-        setErr(validacion)
-    }
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInput({ ...input, [e.target.name]: Number(e.target.value) });
+    const validacion = validate(input, allUsers);
+    setErr(validacion);
+  };
 
-    return (
-        <>
+  return (
+    <>
+      <div className={s.dniContainer}>
+        <h3 onClick={handleClose}>X</h3>
+        <div className={s.dniBody}>
+          <h2>Ingresa tu cédula</h2>
+          <label onSubmit={handleSubmit} className={s.dniForm}>
+            <input
+              value={input.numDocumento}
+              name="numDocumento"
+              onChange={handleChange}
+              type="text"
+              maxLength="10"
+            />
+            {err.numDocumento && (
+              <span className={s.formerror}>{err.numDocumento}</span>
+            )}
 
-            <div className={s.dniContainer}>
-                <h3 onClick={handleClose}>
-                    X
-                </h3>
-                <div className={s.dniBody}>
-                    <h2>Ingresa tu cedula aqui</h2>
-                    <label onSubmit={handleSubmit} className={s.dniForm}>
-                        <input value={input.numDocumento} name="numDocumento" onChange={handleChange} type="text" maxLength="10" />
-                        {err.numDocumento && <span className={s.formerror}>{err.numDocumento}</span>}
-
-                       {isButtonDisabled() ? 
-                       <>
-                       {!err.numDocumento && !yaExiste ? 
-                        <>
-                        <a onClick={handleToggle} style={{'cursor':'pointer'}}>Regístrece aquí</a> 
-                        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                                open={open}>
-                                <UserForm
-                                    handleClose={() => setOpen(false)}
-                                />
-                            </Backdrop>
-                        </>
-                        : null}
-                        </>
-                        :
-                        <button onClick={handleSubmit} disabled={isButtonDisabled()} type='submit'>Acceder</button> }
-
-                    </label>
-                </div>
-            </div>
-        </>
-    )
+            {isButtonDisabled() ? (
+              <>
+                {!err.numDocumento && !yaExiste ? (
+                  <>
+                    <a onClick={handleToggle} style={{ cursor: "pointer" }}>
+                      O regístrate <u>AQUÍ</u>
+                    </a>
+                    <Backdrop
+                      sx={{
+                        color: "#fff",
+                        zIndex: (theme) => theme.zIndex.drawer + 1,
+                      }}
+                      open={open}
+                    >
+                      <UserForm handleClose={() => setOpen(false)} />
+                    </Backdrop>
+                  </>
+                ) : null}
+              </>
+            ) : (
+              <button
+                onClick={handleSubmit}
+                disabled={isButtonDisabled()}
+                type="submit"
+              >
+                Acceder
+              </button>
+            )}
+          </label>
+        </div>
+      </div>
+    </>
+  );
 }
-
